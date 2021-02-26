adb8624e0a87fd6729656699ae64e2d4dd6c0a70
Byron Miguel Cuenca Oviedo <67970251+bmcuenca@users.noreply.github.com>
2021-02-26 10:46:15 -0500

    Add files via upload

---
+++ PRUEBA/src/com/istloja/modelTables/ComunicacionVistaModelosTablas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.istloja.modelTables;
+
+import com.istloja.modelo.Persona;
+import com.istloja.modelo.Proveedores;
+
+/**
+ *
+ * @author johnp
+ */
+public interface ComunicacionVistaModelosTablas {
+   void clickPersona(Persona p);
+   void clickProveedores(Proveedores p);
+   
+}
